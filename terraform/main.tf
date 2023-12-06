0119a43831336dd23b5d19ea81aeeb4e59c0d277
maaqz <92824393+maaqz@users.noreply.github.com>
2023-12-06 12:31:13 +0100

    pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,21 @@
 terraform {
   required_providers {
     linode = {
-      source = "linode/linode"
+      source  = "linode/linode"
       version = "2.10.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.16" #
+    }
+  }
+  backend "s3" {
+    bucket = "linode-lke-cluster-terraform-state"
+    key    = "prod/message-board-api/terraform.tfstate"
+    region = "eu-central-1"
+
+    dynamodb_table = "linode-lke-cluster-lock"
+    encrypt        = true
   }
 }
 
@@ -12,15 +24,15 @@ provider "linode" {
 }
 
 resource "linode_lke_cluster" "message-board-cluster" {
-    label       = "message-board-lke"
-    k8s_version = "1.27"
-    region      = "eu-central"
-    tags        = ["prod"]
+  label       = "message-board-lke"
+  k8s_version = "1.27"
+  region      = "eu-central"
+  tags        = ["prod"]
 
-    pool {
-        type  = "g6-standard-1"
-        count = 1
-    }
+  pool {
+    type  = "g6-standard-1"
+    count = 1
+  }
 
 }
 
