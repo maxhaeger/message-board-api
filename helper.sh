d083f10f6c441ba7a8fc47bfb14ebe85bdf5937b
maxhaeger <maximilian.gross@haeger-consulting.de>
2023-12-14 16:50:13 +0100

    pipeline

--- helper.sh
+++ helper.sh
@@ -2,7 +2,9 @@
 export TF_VAR_linode_token=1df91dc19a5667440497c3dd95562876fa914898b97c62886126cb68f96e24ad
 config=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
 cat $config >> ~/.kube/config.lke
-nb=$(linode-cli nodebalancers list --text --no-header | awk '{print $1'})
+nb=$(for vol in $(linode-cli volumes list --text --no-header | awk '{print $1}'); do
+  linode-cli volumes delete $vol
+done)
 linode-cli nodebalancers delete $nb
 lke=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
 linode-cli lke cluster-delete $lke
