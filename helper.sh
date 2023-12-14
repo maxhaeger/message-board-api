7e76175d0d867ca08de523e4cff26c6abf0d0dc4
maxhaeger <maximilian.gross@haeger-consulting.de>
2023-12-14 14:12:07 +0100

    pipeline

--- helper.sh
+++ helper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export TF_VAR_linode_token=1df91dc19a5667440497c3dd95562876fa914898b97c62886126cb68f96e24ad
-cd terraform && terraform init && terraform apply -auto-approve
-sleep 60 -m "Waiting for Nodes to be ready"
-bash import-kubeconfig.sh
-sleep 5 -m "Waiting for Kubeconfig to be set"
-cd ../helm && helm install backend ./backend && helm install frontend ./frontend && helm install environment ./environment
-sleep 60 -m "Waiting for Pods to be ready"
-kubectl apply -f https://bit.ly/ingress_nginx_1_2_0
+nb=$(linode-cli nodebalancers list --text --no-header | awk '{print $1'})
+linode-cli nodebalancers delete $nb
+lke=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
+linode-cli lke cluster-delete $lke
+vol=$(linode-cli volumes list --text --no-header | awk '{print $1}')
+linode-cli volumes delete $vol
+export LINODE_CLI_TOKEN=1df91dc19a5667440497c3dd95562876fa914898b97c62886126cb68f96e24ad 
+export KUBECONFIG=~/.kube/config.lke
