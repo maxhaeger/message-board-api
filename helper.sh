01d9c11c2abbb1a0df163090ec1e726cba0d23bc
maxhaeger <maximilian.gross@haeger-consulting.de>
2023-12-15 09:23:15 +0100

    pipeline

--- helper.sh
+++ helper.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-export TF_VAR_linode_token=1df91dc19a5667440497c3dd95562876fa914898b97c62886126cb68f96e24ad
+
 config=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
 cat $config >> ~/.kube/config.lke
 nb=$(for vol in $(linode-cli volumes list --text --no-header | awk '{print $1}'); do
-  linode-cli volumes delete $vol
-done)
 linode-cli nodebalancers delete $nb
+linode-cli volumes delete $vol
+done)
 lke=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
 linode-cli lke cluster-delete $lke
 vol=$(linode-cli volumes list --text --no-header | awk '{print $1}')
 linode-cli volumes delete $vol
-export LINODE_CLI_TOKEN=1df91dc19a5667440497c3dd95562876fa914898b97c62886126cb68f96e24ad 
 export KUBECONFIG=~/.kube/config.lke
 
-!/bin/bash
+#!/bin/bash
+set -e
+
+# Überprüfen, ob die benötigten Befehle existieren
+command -v linode-cli >/dev/null 2>&1 || { echo >&2 "linode-cli ist nicht installiert.  Abbruch."; exit 1; }
+command -v base64 >/dev/null 2>&1 || { echo >&2 "base64 ist nicht installiert.  Abbruch."; exit 1; }
+
 configid=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
 config=$(linode-cli lke kubeconfig-view $configid --text --no-headers | base64 -d)
-echo "$config" > ~/.kube/config.lke
-export KUBECONFIG=~/.kube/config.lke
+
+# Verwenden einer temporären Datei
+tempfile=$(mktemp)
+echo "$config" > $tempfile
+export KUBECONFIG=$tempfile
--- setKubeConfig.sh
+++ setKubeConfig.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+set -e
+
+# Überprüfen, ob die benötigten Befehle existieren
+command -v linode-cli >/dev/null 2>&1 || { echo >&2 "linode-cli ist nicht installiert.  Abbruch."; exit 1; }
+command -v base64 >/dev/null 2>&1 || { echo >&2 "base64 ist nicht installiert.  Abbruch."; exit 1; }
+
 configid=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
 config=$(linode-cli lke kubeconfig-view $configid --text --no-headers | base64 -d)
-echo "$config" > ~/.kube/config.lke
-export KUBECONFIG=~/.kube/config.lke
+
+# Verwenden einer temporären Datei anstelle einer Datei im Home-Verzeichnis
+tempfile=$(mktemp)
+echo "$config" > $tempfile
+export KUBECONFIG=$tempfile
