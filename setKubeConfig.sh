ea899339159e4bb1104bcb83d2427b88c0e330e6
maxhaeger <maximilian.gross@haeger-consulting.de>
2023-12-15 15:22:25 +0100

    pipe

--- setKubeConfig.sh
+++ setKubeConfig.sh
@@ -1,9 +1,4 @@
-set -e
-
-# Überprüfen, ob die benötigten Befehle existieren
-command -v linode-cli >/dev/null 2>&1 || { echo >&2 "linode-cli ist nicht installiert.  Abbruch."; exit 1; }
-command -v base64 >/dev/null 2>&1 || { echo >&2 "base64 ist nicht installiert.  Abbruch."; exit 1; }
-
+#!bin/bash
 configid=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
 config=$(linode-cli lke kubeconfig-view $configid --text --no-headers | base64 -d)
 
