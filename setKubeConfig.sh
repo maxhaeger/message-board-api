2726a9d1f984eb936e0dfd7f621bd3eae95e2f12
maxhaeger <maximilian.gross@haeger-consulting.de>
2023-12-19 09:37:39 +0100

    build pipeline

--- setKubeConfig.sh
+++ setKubeConfig.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 configid=$(linode-cli lke clusters-list --text --no-header | awk '{print $1}')
 config=$(linode-cli lke kubeconfig-view $configid --text --no-headers | base64 -d)
 
