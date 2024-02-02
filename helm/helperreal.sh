52a1c565e00595c56f8df7608b20073c09820a70
maxhaeger <maximilian.gross@haeger-consulting.de>
2024-02-02 09:53:03 +0100

    argo test

--- helm/helperreal.sh
+++ helm/helperreal.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if ! k3d cluster list | grep -q "k3s-default"; then
+  k3d cluster create --k3s-arg "--disable=traefik@server:*"
+  echo "Wait to set the pods"
+  sleep 20
+fi
+
 if $(kubectl get namespace argocd >/dev/null 2>&1); then
   echo ArgoCD Namespace Exists
 else
@@ -31,12 +39,23 @@ else
 fi
 echo Environment Installed/Upgraded
 
+external_ip=""
+while [ -z $external_ip ]; do
+  echo "Waiting for end point..."
+  external_ip=$(kubectl get svc ingress-nginx-controller -n ingress-nginx --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+  [ -z "$external_ip" ] && sleep 10
+done
+echo 'End point ready:' && echo $external_ip
+
 if helm status argocd >/dev/null 2>&1; then
   helm upgrade argocd ./argocd -n argocd
 else
   helm install argocd ./argocd -n argocd
 fi
 echo ArgoCD Installed/Upgraded
+sleep 30
+echo Sync Repo with Argo-CRD "Applicaiton"
+kubectl apply -f ../argosetup.yml -n argocd
 
 # if helm status monitoring >/dev/null 2>&1; then
 #   helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
