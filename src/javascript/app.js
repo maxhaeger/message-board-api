95ed2d0d7c01db59ed9dc6ac6baf2906d4c7ec47
maxhaeger <maximilian.gross@haeger-consulting.de>
2024-02-02 11:28:40 +0100

    updated frontend

---
+++ src/javascript/app.js
@@ -0,0 +1,29 @@
+function submitform(evt) {
+    evt.preventDefault();
+    let data = new FormData(evt.target);
+    let title = data.get("title");
+    let message = data.get("message");
+    fetch("/apiv1/messages/create", {
+        method: "POST",
+	headers: {
+		"Content-Type": "application/x-www-form-urlencoded"
+	},
+        body: `title=${title}&message=${message}`
+    }).then(result => {
+        reloadMessages();
+    })
+}
+function reloadMessages() {
+    let listElement = document.getElementById("messages");
+    fetch("/apiv1/messages/list").then(result => {
+        result.json().then(data => {
+            listElement.replaceChildren();
+            data.forEach(msg => {
+                let e = document.createElement("li");
+                e.innerText = `${msg.title}: ${msg.message}`;
+                listElement.appendChild(e);
+            })
+        });
+    });
+}
+window.onload = reloadMessages;
